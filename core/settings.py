4b208eb708ea6b0b6abe56cf7b8c40575d7fafb7
DanielNeacsu-60 <bobibobita064@gmail.com>
2026-07-29 17:38:58 +0300

    Add DATABASES setting

--- core/settings.py
+++ core/settings.py
@@ -56,3 +56,12 @@ WSGI_APPLICATION = 'core.wsgi.application'
 STATIC_URL = 'static/'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
+# Database
+# https://docs.djangoproject.com/en/6.0/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
